341c439bd31e422c1ea3c9a237fd5d5259c5bea5
SIta Dabo <euphoria214rue@gmail.com>
2022-11-28 16:08:13 +0100

    updates

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-const {Telegraf} = require('telegraf')
+
+require('dotenv').config();
+const fs = require('fs')
+const ytdl = require('ytdl-core');
+const {Telegraf} = require('telegraf');
+const { url } = require('inspector');
+const { notDeepEqual } = require('assert');
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 
 
 bot.command('start', ctx=>{
-    console.log(ctx.form);
-    bot.telegram.sendMessage(ctx.chat.id, 'Hello and thank you for using the telegram videodownloader made by @engnrobad, if youw would like to contribute to this project here is a link to the github page:https://github.com/obaddabo/youtube-video-downloader-telegram-bot and also  if you would like to donate to the delopers please do that here : buymeacoffee.com/engnrobad')4
+  
+    bot.telegram.sendMessage(ctx.chat.id, 'Hello and thank you for using the telegram videodownloader made by @engnrobad, if youw would like to contribute to this project here is a link to the github page:https://github.com/obaddabo/youtube-video-downloader-telegram-bot and also  if you would like to donate to the delopers please do that here : buymeacoffee.com/engnrobad')
+    bot.telegram.sendMessage(ctx.chat.id, "enter /download")
 })
 
-bot.hears('hi', (ctx)=> ctx.reply('hello'))
+
+
+
+
+
+
+
+
 
 bot.launch();
 
